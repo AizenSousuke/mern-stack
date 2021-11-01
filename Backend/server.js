d5a1a838d2ef67d8cd2f652b98c09f4e2ba4ece8
AizenSousuke <puteranikdimas@yahoo.com.sg>
2021-11-01 21:50:26 +0800

    Added config and mongoose

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 app.listen(PORT, () => {
 	console.log("Listening on port %s", PORT);
 });
+
+// Connect Database
+connectDB();
+
+// Adds middlewares
 
 app.get('/', (req, res) => {
     res.send(`Server is on port ${PORT}`);
