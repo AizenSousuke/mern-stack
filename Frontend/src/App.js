21a7642eb2b20bb9700de0e34ae2a819ad98f936
AizenSousuke <puteranikdimas@yahoo.com.sg>
2021-11-15 20:09:04 +0800

    Added sample frontend items

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<h1>Title</h1>
+			<Link to="/register">Register</Link>
+			<Routes>
+				<Route path="/" element={<Login />}></Route>
+				<Route path="/register" element={<Register />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ Frontend/src/components/auth/Login.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Login() {
+	return <div>lorem ipsum dolor sit amet, consectetur adipiscing</div>;
+}
