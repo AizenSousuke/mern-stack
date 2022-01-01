7996401cd54034f21952f1fbfe8118e28ca0d5be
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-01-01 22:47:40 +0800

    Connection from react native works using port 8080 instead of 5000 without network error

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 // The express server
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const connectDB = require("./config/db");
 const cors = require("cors");
 const config = require("config");
@@ -24,15 +24,15 @@ const passport = require("passport");
 const FacebookStrategy = require("passport-facebook").Strategy;
 
 // To get HTTPS
-var server = https.createServer(options, app);
-server.listen(PORT, (err, result) => {
-	console.log(`HTTPS Server started on port ${PORT}`);
-});
+// var server = https.createServer(options, app);
+// server.listen(PORT, (err, result) => {
+// 	console.log(`HTTPS Server started on port ${PORT}`);
+// });
 
 // Normal way to get http
-// app.listen(PORT, () => {
-// 	console.log("Listening on port %s", PORT);
-// });
+app.listen(PORT, () => {
+	console.log("Listening on port %s", PORT);
+});
 
 // Connect Database
 connectDB();
@@ -50,15 +50,7 @@ app.use(
 app.use(cookieParser());
 
 // Add CORS
-// app.use(cors({
-// 	credentials: true,
-// 	origin: config.get("FRONTEND_LINK")
-// }));
 app.use(cors());
-// app.use((req, res, next) => {
-// 	res.header('Access-Control-Allow-Origin', config.get("FRONTEND_LINK"));
-// 	next();
-// })
 
 // Passport.js
 app.use(passport.initialize());
--- Frontend/app/screens/GoingOut.js
+++ Frontend/app/screens/GoingOut.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, RefreshControl } from "react-native";
+import { Button } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 import Consumer from "../context/AuthContext";
+import {GetBusStop} from "../api/api";
 
 export default function GoingOut() {
 	const [refreshing, setRefreshing] = useState(false);
@@ -20,9 +22,17 @@ export default function GoingOut() {
 			<Consumer>
 				{(ctx) => {
 					console.log(ctx);
-					return <Text>Token: {JSON.stringify(ctx)}</Text>
+					return <Text>Token: {JSON.stringify(ctx)}</Text>;
 				}}
 			</Consumer>
+			<Button onPress={async () => {
+				console.log("GET");
+				await GetBusStop(44229).then(res => {
+					console.log(res);
+				}).catch(err => {
+					console.error("Error in GET button: " + err);
+				});
+			}}title="GET" />
 		</ScrollView>
 	);
 }
