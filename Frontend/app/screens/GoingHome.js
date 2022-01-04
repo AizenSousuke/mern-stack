93ab65fe6521e49e5450db039b6b89568b52362c
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-01-04 19:10:07 +0800

    Refactored some components

--- Frontend/app/screens/GoingHome.js
+++ Frontend/app/screens/GoingHome.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { RefreshControl } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const GoingHome = (props) => {
+	const [refreshing, setRefreshing] = useState(false);
+
 	return (
-		<View>
-		</View>
+		<ScrollView
+			refreshControl={
+				<RefreshControl
+					refreshing={refreshing}
+					onRefresh={() => {
+						console.log("Refreshing");
+					}}
+				></RefreshControl>
+			}
+		></ScrollView>
 	);
-}
+};
 
 export default GoingHome;
