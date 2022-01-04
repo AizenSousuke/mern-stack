aa9d89682e520a96be18856227fa8e0f7ef6fbb1
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-01-04 22:32:27 +0800

    Refactored Home component

--- Frontend/App.js
+++ Frontend/App.js
@@ -12,73 +12,9 @@ import * as config from "./config/default.json";
 import * as WebBrowser from "expo-web-browser";
 import { AuthProvider } from "./app/context/AuthContext";
 import { SettingsProvider } from "./app/context/SettingsContext";
+import Home from "./app/components/Home";
 
 const Stack = createStackNavigator();
-
-// Don't move or it will cause issues with Tab Navigator
-const Home = ({ navigation }) => {
-	return (
-		<View style={{ flex: 1 }}>
-			<Header
-				placement={"center"}
-				leftComponent={{
-					icon: "logout",
-					color: "white",
-					onPress: async () => {
-						await AsyncStorage.setItem(
-							config.TOKEN,
-							"",
-							(error) => {
-								ToastAndroid.show(error, ToastAndroid.SHORT);
-							}
-						);
-						const result = await LogOut();
-						console.log("Logged out: " + result);
-					},
-				}}
-				centerComponent={{
-					text: "Yet Another SG Bus App",
-					style: { color: "white", fontSize: 18 },
-				}}
-				rightComponent={{
-					icon: "login",
-					color: "white",
-					onPress: async () => {
-						result = await AsyncStorage.getItem(config.TOKEN);
-
-						console.log("Result: " + JSON.stringify(result));
-
-						if (!result) {
-							// Get new token
-							console.log("Signing in");
-							const URL = `${config.BACKEND_API}/auth/facebook`;
-
-							const fblogin = await WebBrowser.openBrowserAsync(
-								URL
-							);
-							if (fblogin) {
-								console.log(
-									"Result: " + JSON.stringify(fblogin)
-								);
-							}
-						} else {
-							ToastAndroid.show(
-								"You have already logged in",
-								1000
-							);
-						}
-					},
-				}}
-			/>
-			<TabNavigator />
-			<SearchButton
-				onPress={() => {
-					navigation.navigate("Search");
-				}}
-			/>
-		</View>
-	);
-};
 
 export default function App() {
 	const [authToken, setAuthToken] = useState(null);
@@ -86,6 +22,8 @@ export default function App() {
 	useEffect(() => {
 		Linking.addEventListener("url", _handleURL);
 		console.log("added event listener");
+		console.log("Looking for token");
+		_loadToken();
 		return () => {
 			// Cleanup
 			Linking.removeAllListeners("url");
@@ -93,15 +31,22 @@ export default function App() {
 		};
 	});
 
+	const _loadToken = async () => {
+		await AsyncStorage.getItem(config.TOKEN, (error, result) => {
+			console.log("_loadToken: " + error + "|" + result);
+			setAuthToken(result);
+		});
+	}
+
 	const _handleURL = async (event) => {
 		console.log("event" + JSON.stringify(event));
 		console.log("Handling URL into app: " + event.url);
 		const token = event.url.split("token=")[1].split("#_=_")[0];
 		console.log("Going to save the token: " + token);
 		console.log("Saving token to async storage");
-		// await AsyncStorage.setItem(config.TOKEN, token.toString(), (error) => {
-		// 	console.error(error);
-		// });
+		await AsyncStorage.setItem(config.TOKEN, token.toString(), (error) => {
+			console.error(error);
+		});
 
 		// Save token
 		setAuthToken(token);
---
+++ Frontend/app/components/Home.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { View, ToastAndroid } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Header } from "react-native-elements";
+import { LogOut } from "../api/api";
+import SearchButton from "../components/SearchButton";
+import TabNavigator from "../components/TabNavigator";
+import * as config from "../../config/default.json";
+import * as WebBrowser from "expo-web-browser";
+
+export const Home = ({ navigation }) => {
+	return (
+		<View style={{ flex: 1 }}>
+			<Header
+				placement={"center"}
+				leftComponent={{
+					icon: "logout",
+					color: "white",
+					onPress: async () => {
+						await AsyncStorage.setItem(
+							config.TOKEN,
+							"",
+							(error) => {
+								ToastAndroid.show(error, ToastAndroid.SHORT);
+							}
+						);
+						const result = await LogOut();
+						console.log("Logged out: " + result);
+					},
+				}}
+				centerComponent={{
+					text: "Yet Another SG Bus App",
+					style: { color: "white", fontSize: 18 },
+				}}
+				rightComponent={{
+					icon: "login",
+					color: "white",
+					onPress: async () => {
+						result = await AsyncStorage.getItem(config.TOKEN);
+
+						console.log("Result: " + JSON.stringify(result));
+
+						if (!result) {
+							// Get new token
+							console.log("Signing in");
+							const URL = `${config.BACKEND_API}/auth/facebook`;
+
+							const fblogin = await WebBrowser.openBrowserAsync(
+								URL
+							);
+                            
+							if (fblogin) {
+								console.log(
+									"Logged in with FACEBOOK: " + JSON.stringify(fblogin)
+								);
+							}
+						} else {
+							ToastAndroid.show(
+								"You have already logged in",
+								1000
+							);
+						}
+					},
+				}}
+			/>
+			<TabNavigator />
+			<SearchButton
+				onPress={() => {
+					navigation.navigate("Search");
+				}}
+			/>
+		</View>
+	);
+};
+
+export default Home;
