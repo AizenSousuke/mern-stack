cd7502d8398209ec20aa8691ca5e680a553c58cd
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-01-10 21:24:00 +0800

    Attempted to add MapBox

--- Frontend/app/screens/LocationModal.js
+++ Frontend/app/screens/LocationModal.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View, Text, Modal } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+import * as config from "../../config/default.json";
+
+MapboxGL.setAccessToken(config.MAPBOX);
 
 export const LocationModal = () => {
 	return (
-		<View>
-		</View>
+		<ScrollView>
+			<MapboxGL.MapView style={{ flex: 1 }} />
+		</ScrollView>
 	);
 };
 
