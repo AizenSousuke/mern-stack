7fb2e1ce0295e8eb533e54a467230c1f59d8d212
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-01-11 14:05:34 +0800

    Can show map now but cannot animate to region

--- Frontend/app/screens/LocationModal.js
+++ Frontend/app/screens/LocationModal.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState, useEffect, createRef, useRef } from "react";
 import { View, Text, Modal } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import MapboxGL from "@react-native-mapbox-gl/maps";
 import * as config from "../../config/default.json";
-
-MapboxGL.setAccessToken(config.MAPBOX);
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
 
 export const LocationModal = () => {
+	const [location, setLocation] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+
+	const mapRef = useRef(null);
+
+	useEffect(() => {
+		(async () => {
+			// await GetLocation();
+		})();
+	}, []);
+
+	const GetLocation = async () => {
+		console.log(Object.keys(mapRef.current));		
+		console.log('mapRef.current.animateToRegion => ', typeof mapRef.current.animateToRegion);
+
+		let { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			setErrorMsg("Permission to access location was denied");
+			return;
+		}
+
+		let location = await Location.getCurrentPositionAsync({});
+		setLocation(location);
+
+		mapRef?.current?.animateToRegion({
+			latitude: location.latitude,
+			longitude: location.longitude,
+			latitudeDelta: 0.01,
+			longitudeDelta: 0.01,
+		}, 100);
+
+		console.log("Location set: " + JSON.stringify(location));
+	};
+
 	return (
 		<ScrollView>
-			<MapboxGL.MapView style={{ flex: 1 }} />
+			<MapView
+				ref={mapRef}
+				provider={PROVIDER_GOOGLE}
+				style={{
+					height: 300,
+					width: 400,
+					justifyContent: "flex-end",
+					alignItems: "center",
+				}}
+				onMapReady={() => GetLocation()}
+				showsUserLocation={true}
+			></MapView>
 		</ScrollView>
 	);
 };
