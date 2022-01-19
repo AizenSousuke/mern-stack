e550ca9f335125682233cec08d7b9dd5a8aee364
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-01-19 14:06:22 +0800

    Added more endpoint testing

--- Backend/routes/api/__Tests__/busstops.test.js
+++ Backend/routes/api/__Tests__/busstops.test.js
@@ -1,13 +1,12 @@
 const request = require("supertest");
 const app = require("../../../server");
 const api = request(app);
-
+// jest.setTimeout(10000);
 // Note: Remember to use {} in test.json in config folder
 
 describe("/BusStops Test", () => {
 	it("should return a Bus Stop data with provided with a code query string", (done) => {
-		api
-			.get("/api/BusStops")
+		api.get("/api/BusStops")
 			.query({ code: 44229 })
 			.expect(200)
 			.end((err, res) => {
@@ -20,8 +19,7 @@ describe("/BusStops Test", () => {
 	});
 
 	it("should return 422 when there is no code query string provided", (done) => {
-		api
-			.get("/api/BusStops")
+		api.get("/api/BusStops")
 			.expect(422)
 			.end((err, res) => {
 				if (err) {
@@ -32,14 +30,37 @@ describe("/BusStops Test", () => {
 	});
 
 	it("should return 404 when the bus stop doesn't exists", (done) => {
-		api
-			.get("/api/BusStops")
+		api.get("/api/BusStops")
 			.query({ code: 12345 })
 			.end((err, res) => {
 				if (err) {
 					return done(err);
 				}
 				expect(res.status).toBe(404);
+				done();
+			});
+	});
+
+	it("should get the list of nearest bus stop based on lat and long provided", (done) => {
+		api.get("/api/BusStops/nearest")
+			.query({ latitude: 1.377725, longitude: 103.7699767 })
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				expect(res.body.busStopsNearby.length).toBeGreaterThan(0);
+				done();
+			});
+	});
+
+	it("should get an empty list of nearest bus stop based on lat and long provided outside Singapore", (done) => {
+		api.get("/api/BusStops/nearest")
+			.query({ latitude: 10.377725, longitude: 103.7699767 })
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+				expect(res.body.busStopsNearby.length).toBe(0);
 				done();
 			});
 	});
