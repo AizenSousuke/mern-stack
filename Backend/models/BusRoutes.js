063a3c07b410900271b66b929e07c342dee2e244
Nik <nik@commerce.com.sg>
2022-02-07 16:43:20 +0800

    Added Bus Routes Schema

---
+++ Backend/models/BusRoutes.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const BusRoutesSchema = mongoose.Schema({
+    ServiceNo: {
+        type: String,
+        required: true
+    },
+    Operator: {
+        type: String,
+        required: true
+    },
+    Direction: {
+        type: Number,
+        required: true
+    },
+    StopSequence: {
+        type: Number,
+        required: true
+    },
+    BusStopCode: {
+        type: Number,
+        required: true
+    },
+    Distance: {
+        type: Number,
+        required: true
+    },
+    WD_FirstBus: {
+        type: Number,
+        required: true
+    },
+    WD_LastBus: {
+        type: Number,
+        required: true
+    },
+    SAT_FirstBus: {
+        type: Number,
+        required: true
+    },
+    SAT_LastBus: {
+        type: Number,
+        required: true
+    },
+    SUN_FirstBus: {
+        type: Number,
+        required: true
+    },
+    SUN_LastBus: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = BusRoute = mongoose.model("busroutes", BusRoutesSchema);
