50f86006661be2d4187e0c5e1dd7158763ae066a
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-02-28 22:53:18 +0800

    Added bus information

--- Frontend/app/components/Table.js
+++ Frontend/app/components/Table.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-elements";
 
-const Table = (data, columnWidth = 1) => {
+const Table = ({ information }) => {
 	return (
-		<View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
-			{/* {data.map((row) => {
+		<View style={{ display: "flex" }}>
+			{information.map((info) => {
 				return (
-					<View style={{ flex: columnWidth }}>
-						{JSON.stringify(row)}
+					<View
+						style={{
+							display: "flex",
+						}}
+					>
+						<View
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<Text>WD First Bus: {info.WD_FirstBus} hrs</Text>
+							<Text>WD Last Bus: {info.WD_LastBus} hrs</Text>
+						</View>
+						<View
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<Text>SAT First Bus: {info.SAT_FirstBus} hrs</Text>
+							<Text>SAT Last Bus: {info.SAT_LastBus} hrs</Text>
+						</View>
+						<View
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<Text>SUN First Bus: {info.SUN_FirstBus} hrs</Text>
+							<Text>SUN Last Bus: {info.SUN_LastBus} hrs</Text>
+						</View>
 					</View>
 				);
-			})} */}
-			<View style={{ flex: 1 }}>
-				{/* <Text>{JSON.stringify(data)}</Text> */}
-			</View>
+			})}
 		</View>
 	);
 };
