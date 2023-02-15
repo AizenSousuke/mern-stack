90eaa704c967c48681b3598c0c8ec7512119cbb2
Nik <puteranda_nik@certisgroup.com>
2023-02-15 13:21:42 +0800

    Updated test for bus stop saved

--- Frontend/app/__Tests__/Components/BusStopSaved.test.js
+++ Frontend/app/__Tests__/Components/BusStopSaved.test.js
@@ -1,17 +1,20 @@
-import { render } from "@testing-library/react-native";
 import { act, create } from "react-test-renderer";
 import BusStopSaved from "../../components/BusStopSaved";
 
 jest.mock("../../api/api");
 
 describe("BusStopSaved", () => {
-	it("renders properly", () => {
-		render(<BusStopSaved />);
+	it("renders properly", async () => {
+		await act(async () => {
+			await create(<BusStopSaved />);
+		});
 	});
 
-	it("renders properly according to snapshot", () => {
-		const snapshot = render(<BusStopSaved />).toJSON();
-		expect(snapshot).toMatchSnapshot();
+	it("renders properly according to snapshot", async () => {
+		await act(async () => {
+			const snapshot = (await create(<BusStopSaved />)).toJSON();
+			expect(snapshot).toMatchSnapshot();
+		});
 	});
 
 	it("renders with the correct data", async () => {
