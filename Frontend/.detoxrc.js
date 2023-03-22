9e5967e6b0675ebe27ca955f7348e907f25abaa1
Nik <puteranda_nik@certisgroup.com>
2023-03-22 13:35:07 +0800

    Edited proguard rules

--- Frontend/.detoxrc.js
+++ Frontend/.detoxrc.js
@@ -39,6 +39,7 @@ module.exports = {
     'android.release.mac': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      testBinaryPath: 'android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
       build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release --warning-mode all && cd ..',
       reversePorts: [
         8081
@@ -47,6 +48,7 @@ module.exports = {
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      testBinaryPath: 'android/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
       build: 'cd android && .\\gradlew assembleRelease assembleAndroidTest -DtestBuildType=release --warning-mode all && cd ..',
       reversePorts: [
         8081
