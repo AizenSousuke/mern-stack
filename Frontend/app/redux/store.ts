3a01c76858392d3a573374ad658bbee67a9ddf98
AizenSousuke <puteranikdimas@yahoo.com.sg>
2024-06-02 21:08:39 +0800

    Added persist store example

--- Frontend/app/redux/store.ts
+++ Frontend/app/redux/store.ts
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers/rootReducer";
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
-});
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+    let store = configureStore({
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    });
+
+    let persistedStore = persistStore(store);
+
+    return { store, persistedStore }
+}
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,14 +1,21 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 import { Provider } from "react-redux";
-import { store } from "./app/redux/store";
-
-import App from './App';
+import createStore from "./app/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
 
 const rootApp = () => {
-    return <Provider store={store}>
-        <App />
-    </Provider>
-}
+	return (
+		<Provider store={createStore().store}>
+			<PersistGate
+				loading={null}
+				persistor={createStore().persistedStore}
+			>
+				<App />
+			</PersistGate>
+		</Provider>
+	);
+};
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
