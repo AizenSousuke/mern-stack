7462ba5b0ae239032dc54a0e03baef942163c755
Nik <puteranda_nik@certisgroup.com>
2024-07-15 17:36:21 +0800

    Added more functions

--- Frontend/app/redux/features/homePage/homePageSlice.ts
+++ Frontend/app/redux/features/homePage/homePageSlice.ts
@@ -2,11 +2,18 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import api from "../../../api/api"
 
 const initialState = {
-    isLoggedIn: false
+    isLoggedIn: false,
+    token: null,
+    goingOut: [],
+    goingHome: []
 }
 
 export const signIn = createAsyncThunk('Home/signIn', async () => {
     return await api.SignIn();
+})
+
+export const getSettings = createAsyncThunk('Home/getSettings', async (token: string) => {
+    return await api.GetSettings(token);
 })
 
 export const homePageSlice = createSlice({
@@ -15,12 +22,25 @@ export const homePageSlice = createSlice({
     reducers: {
         loggedIn: (state, action) => {
             state.isLoggedIn = action.payload;
+        },
+        setToken: (state, action) => {
+            state.token = action.payload;
+        },
+        goingOut: (state, action) => {
+            state.goingOut = action.payload;
+        },
+        goingHome: (state, action) => {
+            state.goingHome = action.payload;
         }
     },
     extraReducers: (builder) => {
         builder.addCase(signIn.fulfilled, (state, action) => {
-        })
+        });
+        builder.addCase(getSettings.fulfilled, (state, action) => {
+            state.goingOut = action.payload.settings?.Settings?.GoingOut;
+            state.goingHome = action.payload.settings?.Settings?.GoingHome;
+        });
     }
 })
 
-export const { loggedIn } = homePageSlice.actions;
+export const { loggedIn, goingOut, goingHome } = homePageSlice.actions;
