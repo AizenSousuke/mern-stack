22a6d4e196410b43ea7d771959125023c39230ae
AizenSousuke <puteranikdimas@yahoo.com.sg>
2024-08-01 18:55:18 +0800

    Updated async storage mock

--- Frontend/__mocks__/@react-native-async-storage/async-storage.js
+++ Frontend/__mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,8 @@
-export * from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+const mockAsyncStorage = {
+	getItem: jest.fn(() => Promise.resolve(JSON.stringify({}))),
+	setItem: jest.fn(() => Promise.resolve(null)),
+	removeItem: jest.fn(() => Promise.resolve(null)),
+	clear: jest.fn(() => Promise.resolve(null)),
+};
+
+export default mockAsyncStorage;
--- Frontend/app/redux/store.ts
+++ Frontend/app/redux/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers/rootReducer";
 import logger from 'redux-logger';
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const persistConfig = {
@@ -13,7 +13,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    }).concat(logger)
 });
 
 export const persistedStore = persistStore(store);
