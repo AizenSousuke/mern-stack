749c6e19031d60c9a0bc4e18806972b1bd2552a2
Nik <puteranda_nik@certisgroup.com>
2024-08-09 18:31:51 +0800

    Updated db from js to ts

---
+++ Backend/db.ts
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const configuration = require("config");
+const db = process.env.mongoURI ?? configuration.get("mongoURI");
+
+export const connectDB = async () => {
+	try {
+		await mongoose.connect(db);
+		console.log("MongoDB Connected...");
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
