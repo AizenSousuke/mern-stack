9c372ec8ec23f5107bcefe146d78ba272fe0e215
Nik <puteranda_nik@certisgroup.com>
2024-09-16 08:09:08 +0800

    Changed user ts

--- Backend/routes/api/users.ts
+++ Backend/routes/api/users.ts
@@ -6,15 +6,17 @@ const User = require("../../models/User").default;
 import bcrypt from "bcryptjs";
 import config from "config";
 import jwt from "jsonwebtoken";
+import PrismaSingleton from "../../classes/PrismaSingleton";
+const prisma = PrismaSingleton.getPrisma();
 
 router.get("/", async (req, res) => {
 	try {
-		const users = await UserModel.find({});
+		const users = await prisma.user.findMany({});
 
 		if (!users || users.length === 0) {
 			return res.status(404).json({ msg: "No user(s) found" });
 		}
-		
+
 		return res
 			.status(200)
 			.json({ msg: "Successfully got the user", users: users });
@@ -41,95 +43,70 @@ router.post(
 		}
 		const { Name, Email, Password } = req.body;
 
-		// Session
-		const session = await UserModel.startSession();
-		// Transaction
-		session.startTransaction();
-		try {
-			// See if user exists
-			let user = await UserModel.findOne({ Email }).session(session);
+		// See if user exists
+		let user = await prisma.user.findFirst({ where: { email: Email } });
 
-			if (user) {
-				await session.endSession();
-				return res.status(400).json({
-					errors: [{ msg: "User already exists" }],
-				});
-			}
-
-			// Get users gravatar
-			const Avatar = gravatar.url(Email, {
-				s: "200",
-				r: "pg",
-				d: "mm",
+		if (user) {
+			return res.status(400).json({
+				errors: [{ msg: "User already exists" }],
 			});
-
-			user = new User({ Name, Email, Avatar, Password });
-
-			// Encrypt password
-			const salt = await bcrypt.genSalt(10);
-			user.Password = await bcrypt.hash(Password, salt);
-			await user.save({ session });
-
-			// Return jsonwebtoken (so user can log in straightaway)
-			const payload = {
-				user: {
-					id: user.UserId,
-				},
-			};
-
-			jwt.sign(
-				payload,
-				process.env.jwtSecret ?? config.get("jwtSecret"),
-				{ expiresIn: 3600000 },
-				(error, token) => {
-					if (error) throw error;
-					res.json({ token });
-				}
-			);
-			await session.commitTransaction();
-			await session.endSession();
-		} catch (error) {
-			console.error(error.message);
-			await session.abortTransaction();
-			await session.endSession();
-			return res.status(500).json({ msg: "Server error" });
 		}
+
+		// Get users gravatar
+		const Avatar = gravatar.url(Email, {
+			s: "200",
+			r: "pg",
+			d: "mm",
+		});
+
+		user = new User({ Name, Email, Avatar, Password });
+
+		// Encrypt password
+		const salt = await bcrypt.genSalt(10);
+
+		await prisma.user.update({
+			where: {
+				id: user.id
+			},
+			data: {
+				password: await bcrypt.hash(Password, salt)
+			}
+		});
+
+		// Return jsonwebtoken (so user can log in straightaway)
+		const payload = {
+			user: {
+				id: user.id,
+			},
+		};
+
+		jwt.sign(
+			payload,
+			process.env.jwtSecret ?? config.get("jwtSecret"),
+			{ expiresIn: 3600000 },
+			(error, token) => {
+				if (error) throw error;
+				res.json({ token });
+			}
+		);
 	}
 );
 
 router.delete("/", async (req, res) => {
-	const email = req.query.email;
+	const email = req.query.email.toString();
 
 	if (!email) {
 		return res.status(422).json({ err: "Invalid email address" });
 	}
+	let user = await prisma.user.findFirst({ where: { email: email } });
 
-	// Session
-	const session = await UserModel.startSession();
-	try {
-		// Transaction
-		session.startTransaction();
-
-		let user = await UserModel.findOne({ Email: email }).session(session);
-
-		if (!user) {
-			await session.endSession();
-			return res.status(404).json({ msg: "User not found" });
-		}
-
-		await user.remove({ session });
-
-		await session.commitTransaction();
-		await session.endSession();
-		return res
-			.status(200)
-			.json({ msg: `Removed user with email: ${email}` });
-	} catch (error) {
-		console.error(error.message);
-		await session.abortTransaction();
-		await session.endSession();
-		return res.status(500).json({ msg: "Server error" });
+	if (!user) {
+		return res.status(404).json({ msg: "User not found" });
 	}
+
+	return res
+		.status(200)
+		.json({ msg: `Removed user with email: ${email}` });
 });
 
 export default router;
