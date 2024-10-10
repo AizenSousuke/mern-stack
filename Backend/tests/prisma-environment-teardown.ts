a8477e228d3e050d78e0083c3e12721d7996bbaf
Nik <puteranda_nik@certisgroup.com>
2024-10-10 12:06:49 +0800

    Can run test now

--- Backend/tests/prisma-environment-teardown.ts
+++ Backend/tests/prisma-environment-teardown.ts
@@ -1,8 +1,8 @@
 
 module.exports = async ({ mongod }) => {
-    // const { PrismaClient } = require('@prisma/client');
-    // const prisma = new PrismaClient();
-    // console.log("Typeof prisma:", typeof prisma);
+    const { PrismaClient } = require('@prisma/client');
+    const prisma = new PrismaClient();
+    console.log("Typeof prisma:", typeof prisma);
     await prisma.$disconnect();
     if (mongod) await mongod.stop();
 };
